0add4596ee9974f5b62c6f089da31afef4224162
yuling111 <waini1023@gmail.com>
2021-07-28 17:44:26 +0800

    滑鼠左鍵雙擊畫圓，點右鍵畫矩形

--- ch1/Mouse.py
+++ ch1/Mouse.py
@@ -1,12 +1,16 @@
 import numpy as np
 import cv2 as cv
+
 # 鼠标回调函数
+
 def draw_circle(event,x,y,flags,param):
     if event == cv.EVENT_LBUTTONDBLCLK:
-        cv.circle(img,(x,y),100,(255,0,0),-1)
+        cv.circle(img,(x,y),10,(255,0,0),-1)
 # 创建一个黑色图像，一个窗口，然后和回调绑定
 img = np.zeros((512,512,3), np.uint8)
 cv.namedWindow('image')
+
+#抓滑鼠的動作cv.setMouseCallback(視窗, 要做什麼動作(觸發哪個funtion))
 cv.setMouseCallback('image',draw_circle)
 
 while(1):
