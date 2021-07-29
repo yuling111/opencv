fa60a61e22a17ea646af1319180ab36f0bde48aa
yuling111 <waini1023@gmail.com>
2021-07-29 17:12:47 +0800

    去背cv2.grabCut( )

--- ch4-imageProccessing/bgdelete.py
+++ ch4-imageProccessing/bgdelete.py
@@ -6,13 +6,14 @@ src = cv2.imread('images3.jpg')
 img = src.copy()
 
 mask = np.zeros((img.shape[:2]), np.uint8)
-bgdModel = np.zeros((3, 65), np.float64)
-fgdModel = np.zeros((3, 65), np.float64)
+bgdModel = np.zeros((1, 65), np.float64)
+fgdModel = np.zeros((1, 65), np.float64)
 # 设置矩形位置
 rect = (425, 200, 834, 500)
 cv2.grabCut(img, mask, rect, bgdModel, fgdModel, 5, cv2.GC_INIT_WITH_RECT)
 mask2 = np.where((mask == 2) | (mask == 0), 0, 1).astype('uint8')
 img = img*mask2[:, :, np.newaxis]
+
 
 plt.subplot(121), plt.imshow(cv2.cvtColor(src, cv2.COLOR_BGR2RGB)),
 plt.title('Original'), plt.axis('off')
@@ -20,3 +21,7 @@ plt.subplot(122), plt.imshow(cv2.cvtColor(img, cv2.COLOR_BGR2RGB))
 plt.title('Target'), plt.axis('off')
 
 plt.show()
+
+'''
+GRAB完，將背景透明參考code -> bgSheer.py
+'''
